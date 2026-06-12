90a6cc6f61f2654c0bdf7eba2a572046f7c2960d
Sanesh <sanesh7644@gmail.com>
2026-06-12 11:06:05 +0530

    database connection using atlus mongose

--- src/index.js
+++ src/index.js
@@ -8,21 +8,14 @@ import {DB_NAME} from "./constants.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 connectDB();
 
 
 
 
-
-
-
-
-
-
-
- //method 1
+//method 1
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants.js";
 // import express from "express";
