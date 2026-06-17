f8bd6843d7c140dd7b89922ce5cf1bdb6177c2aa
Sanesh <sanesh7644@gmail.com>
2026-06-17 11:36:50 +0530

    Add tweet, comment, dashboard, health check, like, playlist, subscription, and video controllers and routes

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
 
+
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/users",tweetRouter);
 
 //routers declaration
 //app.use("/api/v1/users",userRouter)
